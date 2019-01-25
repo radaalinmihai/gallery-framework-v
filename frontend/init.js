1002e62c1d2a5402c7b41fba4c579d9a3741b23f
DontStealMyCodePls <n3gatyv95@gmail.com>
2019-01-26 01:27:34 +0200

    Added socket.io and configured the static files + carousel completed

--- frontend/init.js
+++ frontend/init.js
@@ -1,53 +1,152 @@
 function init (containerID, params) {
-	switch (params.type) { // check which format is asked
+	switch (params.type) { // check which format is requested
 		case "carousel":
-			var members = document.getElementsByClassName("member"); // get list of elements
 			var container = document.getElementById(containerID); // get container
 			container.className += " carousel-container"; 
 			var stage = container.firstElementChild; // get stage
+			var members = document.getElementsByClassName("member"); // get list of elements
 			stage.className += " car-stage";
 			var containerWidth = container.offsetWidth; // width of container at time of init
 			var i;
 
 			if (params.hasOwnProperty("items")) {
 				if (params.items != null) {
+					var itemsPerSlide = params.items;
 				}
 			}
+			else var itemsPerSlide = 3; // carousel defaults to 3 items per slide
 
-			var memberWidth = containerWidth / params.items;
+			var memberWidth = containerWidth / itemsPerSlide;
 			for (i = 0; i < members.length; i++) {
 				members[i].style.width = memberWidth.toString() + "px";
 			}
-			var stageWidth = containerWidth * (members.length / params.items);
+			var stageWidth = containerWidth * (members.length / itemsPerSlide);
 			stage.style.width = stageWidth.toString() + "px";
 
-			if ()
-			function slideAnim() { // move an entire slide at once
-				stage.style.transform = "translateX(0px)";
-				var prev = document.getElementById("nav-prev");
-				var next = document.getElementById("nav-next");
-				var transformSize = 0;
-
-				prev.onclick = function() {
-					if (transformSize != 0) {
-						transformSize -= containerWidth;
-						stage.style.transform = "translateX(-" + transformSize.toString() + "px)";
-					}
+			if (params.hasOwnProperty("transition")) {
+				if (params.transition != null) {
+					animateTransition(params.transition, stage, stageWidth, containerWidth, memberWidth, container);
 				}
-				next.onclick = function() {
-					if (transformSize < (stageWidth - containerWidth)) {
-						transformSize += containerWidth;
-						stage.style.transform = "translateX(-" + transformSize.toString() + "px)";
-					}
+				else animateTransition("slide", stage, stageWidth, containerWidth, memberWidth, container); // defaults to slide
+			}
+	}
+}
+
+function error (missingPar) {
+	console.log("You have to add the property for " + missingPar + " in order for it to work.");
+}
+
+function animateTransition(options, stage, stageWidth, containerWidth, memberWidth, container) {
+	if (options.hasOwnProperty("transitionItems")) {
+		if (options.transitionItems != null) {
+			var transitionItemsNum = options.transitionItems;
+		}
+	}
+	else var transitionItemsNum = 1; // carousel defaults to 1 item per transition
+
+	var mouseover = 1;
+
+	container.onmouseover = function() {
+		mouseover = 0;
+	}
+
+	container.onmouseout = function() {
+		mouseover = 1;
+	}
+
+	stage.style.transform = "translateX(0px)";
+	var prev = document.getElementById("nav-prev");
+	var next = document.getElementById("nav-next");
+	var transformSize = 0;
+
+	switch (options.transitionType) {
+		case "slide":
+
+			prev.onclick = function() {
+				if (transformSize != 0) {
+					transformSize -= containerWidth;
+					stage.style.transform = "translateX(-" + transformSize.toString() + "px)";
+				}
+			}
+			next.onclick = function() {
+				if (transformSize < (stageWidth - containerWidth)) {
+					transformSize += containerWidth;
+					stage.style.transform = "translateX(-" + transformSize.toString() + "px)";
 				}
 			}
 
-			slideAnim();
+			if (options.hasOwnProperty("auto")) {
+				if (options.auto != null) {
+					if (options.auto == "true") {
+						if (options.hasOwnProperty("autoInterval")) {
+							if (options.autoInterval != null) {
+								setInterval(function() {
+									if (mouseover == 1) {
+										if (transformSize < (stageWidth - containerWidth)) {
+											transformSize += containerWidth;
+											stage.style.transform = "translateX(-" + transformSize.toString() + "px)";
+										}
+									}
+								}, options.autoInterval);
+							}
+						}
+						else error("autoInterval");
+					}
+				}
+			}
+			else error("auto");
 
-			break;
+		break;
+
+		case "item":
+
+			prev.onclick = function() {
+				if (transformSize != 0) {
+					transformSize -= memberWidth * transitionItemsNum;
+					stage.style.transform = "translateX(-" + transformSize.toString() + "px)";
+				}
+			}
+			next.onclick = function() {
+				if (transformSize < (stageWidth - containerWidth)) {
+					transformSize += memberWidth * transitionItemsNum;
+					stage.style.transform = "translateX(-" + transformSize.toString() + "px)";
+				}
+			}
+
+			if (options.hasOwnProperty("auto")) {
+				if (options.auto != null) {
+					if (options.auto == "true") {
+						if (options.hasOwnProperty("autoInterval")) {
+							if (options.autoInterval != null) {
+								setInterval(function() {
+									if (mouseover == 1) {
+										if (transformSize < (stageWidth - containerWidth)) {
+											transformSize += memberWidth * transitionItemsNum;
+											stage.style.transform = "translateX(-" + transformSize.toString() + "px)";
+										}
+									}
+								}, options.autoInterval);
+							}
+						}
+						else error("autoInterval");
+					}
+				}
+			}
+			else error("auto");
+
+		break;
 	}
 }
 	
 document.addEventListener('DOMContentLoaded', function() {
-    init("carousel1", {items:3, type:"carousel"});
+    init("carousel1", {
+    	items:4, 
+    	type:"carousel", 
+    	transition:{
+    		transitionType:"item", 
+    		auto:"true", 
+    		autoInterval:1500, 
+    		transitionItemsNum: 1
+    	}
+    });
 }, false);
--- server.js
+++ server.js
@@ -1,9 +1,25 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var socket = require('socket.io')(http);
+var bcrypt = require('bcrypt-nodejs');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/frontend/'));
 
 app.get('/', (req, res) => {
-    res.send('Hello');
+    res.sendFile(__dirname + '/frontend/index.html');
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/frontend/register.html');
+    socket.on('connection', (socket) => {
+        console.log('a user connected');
+        // urmeaza sa fac ceva cu cacatul asta de socket.io, sa primesc raspuns live or some shit
+    });
 });
 
 http.listen(3000, (req, res) => {
