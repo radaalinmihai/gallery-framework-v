ab0967e68cf680bec92d44183dd86547ab9c39b1
DontStealMyCodePls <n3gatyv95@gmail.com>
2019-01-25 21:03:38 +0200

    Updating commit

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 var app = require('express')();
+var http = require('http').Server(app);
+var socket = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+    res.send('Hello');
+});
+
+http.listen(3000, (req, res) => {
+    console.log('Listening on port 3000');
+});
