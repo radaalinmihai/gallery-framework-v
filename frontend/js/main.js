14fb1115917f35a9284d0a4e864bf349713d26f7
aflaviusandrei <aflaviusandrei@gmail.com>
2019-03-28 21:24:29 +0200

    Created redirect to token visualization page via token input field on main page

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,6 +1,15 @@
 var menu = document.getElementById('open-burger');
 var exit = document.getElementById('exit');
 var navm = document.getElementById('nav');
+var tokenInput = document.getElementsByClassName("token-input")[0];
+
+tokenInput.addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        console.log(tokenInput.value);
+        window.location = "/album?token=" + tokenInput.value;
+    }
+});
 
 menu.addEventListener('click', function (e) {
   navm.classList.toggle('open-menu');
