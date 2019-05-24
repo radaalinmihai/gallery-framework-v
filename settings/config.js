4e5617cd8cd8398fed3adb0eb6010b2020d315ff
DontStealMyCodePls <n3gatyv95@gmail.com>
2019-05-24 23:45:12 +0300

    Modified the database

--- settings/config.js
+++ settings/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    database: 'mongodb+srv://reversio:elcomandante@edociif-5wsli.gcp.mongodb.net/test?retryWrites=true'
+    database: 'mongodb+srv://rada:wingmanomul123@gitnephew-5wsli.gcp.mongodb.net/test?retryWrites=true'
 }
