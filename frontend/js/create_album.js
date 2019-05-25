ab935c7cbf384e5e56557311c123b329b95e610d
reversio <ionutsouless@gmail.com>
2019-05-25 06:33:48 +0000

    Update create_album.js

--- frontend/js/create_album.js
+++ frontend/js/create_album.js
@@ -20,7 +20,7 @@ function create_album(e) {
     }
 
     if(error == 0)
-        ajax('http://localhost:3000/create_album', {
+        ajax('https://edociif.netlify.com/create_album', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
